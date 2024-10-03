9ef50c3cbd047c6dfd7ceeb9eb039a927360e47c
Ivan Avila <ivanavilar456@gmail.com>
2024-10-02 20:16:23 -0600

    Refactorizar sistema de permisos y roles a Livewire
    - Mover Permisos.php y Roles.php de Models a Livewire
    - Eliminar antiguos modelos Permiso.php y Rol.php
    - Crear nuevo componente Livewire ModalPermisos

--- app/Livewire/Permisos.php
+++ app/Livewire/Permisos.php
@@ -3,9 +3,17 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Spatie\Permission\Models\Permission;
 
 class Permisos extends Component
 {
+    public $permissions;
+
+    public function mount ()
+    {
+        $this->permissions = Permission::all();
+    }
+
     public function render()
     {
         return view('livewire.permisos');
--- app/Livewire/Roles.php
+++ app/Livewire/Roles.php
@@ -3,9 +3,18 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Volt\Compilers\Mount;
+use Spatie\Permission\Models\Role;
 
 class Roles extends Component
 {
+    public $role;
+
+    public function mount()
+    {
+        $this->role = Role::all();
+    }
+    
     public function render()
     {
         return view('livewire.roles');
